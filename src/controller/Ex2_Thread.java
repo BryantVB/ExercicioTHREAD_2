06d7c6f7bef882c0be7d1064abca3014a6e4cfc1
Bryan <Bryan@DESKTOP-O0TJ40G>
2022-03-04 20:46:28 -0300

    EXERCICIO 2

---
+++ src/controller/Ex2_Thread.java
@@ -0,0 +1,21 @@
+    package controller;
+    public class Ex2_Thread extends Thread {
+	private int n;
+	private int MT[][];
+
+	public Ex2_Thread(int n, int MT[][]) {
+		this.n = n;
+		this.MT = MT;
+	}
+	@Override
+	public void run() {
+		Mostrar();
+	}
+    public void Mostrar() {
+		int soma = 0;
+		for (int c = 0; c < 5; c++) {
+			soma += MT[n][c];
+		}
+		System.out.println("Soma da linha " + (n + 1) + " da matriz= " + soma);
+	}
+}
